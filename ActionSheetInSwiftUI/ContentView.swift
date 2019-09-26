80c4421f94fb61931635a39cb6c119b2a7f1dd1d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-26 13:29:45 +0300

    init

--- ActionSheetInSwiftUI/ContentView.swift
+++ ActionSheetInSwiftUI/ContentView.swift
@@ -9,8 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isVisible = false
     var body: some View {
-        Text("Hello World")
+        Button(action: {
+            self.isVisible.toggle()
+        }) {
+            Text("Show Action Sheet")
+        }.actionSheet(isPresented: $isVisible) {
+            ActionSheet(title: Text("Welcome to Action Sheet tutorial"), message: Text("This is simple exaple on how to use an action sheet"), buttons: [
+                ActionSheet.Button.default(Text("Share location")),
+                ActionSheet.Button.default(Text("Rate")),
+                ActionSheet.Button.destructive(Text("Report abuse"), action: {
+                    print("This button was clicked")
+                })
+            ])
+        }
     }
 }
 
